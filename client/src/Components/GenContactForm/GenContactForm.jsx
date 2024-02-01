a41d3947ddb5671a8da013deb3c4afb03b44344e
technofra-govind <govind@technofra.com>
2024-02-01 13:20:49 +0530

    update the apilink

--- client/src/Components/GenContactForm/GenContactForm.jsx
+++ client/src/Components/GenContactForm/GenContactForm.jsx
@@ -25,7 +25,7 @@ function GenContactForm() {
     try {
       setSubmitting(true);
 
-      const response = await fetch('https://mahesh-backend-deploy.onrender.com', {
+      const response = await fetch('https://mahesh-backend-deploy.onrender.com/submit-form', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
